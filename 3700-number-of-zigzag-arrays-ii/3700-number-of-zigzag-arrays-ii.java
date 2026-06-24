29dea7b82209415e1eb1fb043dd9080e8d1ac66c
Sohayel Mahmud <132913772+sohayelmahmud@users.noreply.github.com>
2026-06-24 06:31:22 +0500

    Date: 06-24-2026, Lang: .java, Topic:  Math

---
+++ 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public int zigZagArrays(int n, int l, int r) {
+        long[][] a = new long[r - l][r - l];
+        long[][] b = new long[r - l][r - l];
+        long result = 0;
+        for (int i = 0; i < r - l; i++) {
+            a[i][i] = 1;
+            for (int j = r - l - 1; i + j >= r - l - 1 && j >= 0; j--) {
+                b[i][j] = 1;
+            }
+        }
+        for (n--; n > 0; n /= 2) {
+            if (n % 2 == 1) {
+                a = zigZagArrays(a, b);
+            }
+            b = zigZagArrays(b, b);
+        }
+        for (int i = 0; i < r - l; i++) {
+            for (int j = 0; j < r - l; j++) {
+                result += a[i][j];
+            }
+        }
+        return (int) (result * 2 % 1000000007);
+    }
+
+    private long[][] zigZagArrays(long[][] a, long[][] b) {
+        long[][] c = new long[a.length][a.length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                for (int k = 0; k < a.length; k++) {
+                    c[i][j] = (c[i][j] + a[i][k] * b[k][j]) % 1000000007;
+                }
+            }
+        }
+        return c;
+    }
+}
